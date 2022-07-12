9738d6c4690a5b5c0008210ad3679046d2daafb4
Eric Flumerfelt <eflumerf@fnal.gov>
2022-07-12 15:40:52 -0500

    Fix Doxygen header

--- pybindsrc/hardwaremapservice.cpp
+++ pybindsrc/hardwaremapservice.cpp
@@ -1,5 +1,5 @@
 /**
- * @file wib.cpp
+ * @file hardwaremapservice.cpp
  *
  * This is part of the DUNE DAQ Software Suite, copyright 2020.
  * Licensing/copyright details are in the COPYING file that you should have
